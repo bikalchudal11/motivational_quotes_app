67fd3dadda8494c0c34474dd39a1c5468506fa1e
Bikal Chudal <bikalchudal11@gmail.com>
2024-04-22 06:45:50 +0545

    done ui for home screen

--- lib/view/screens/home_page.dart
+++ lib/view/screens/home_page.dart
@@ -1,12 +1,47 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, sized_box_for_whitespace
 
 import 'package:flutter/material.dart';
+import 'package:http/retry.dart';
+import 'package:motivational_quotes_app/view/resources/color.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Quotes for the day",
+        ),
+        centerTitle: true,
+        backgroundColor: pColor,
+        foregroundColor: Colors.white,
+      ),
+      body: ListView.builder(
+          itemCount: 5,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: Container(
+                      height: 100,
+                      width: MediaQuery.of(context).size.width,
+                      child: Center(
+                        child: Text(
+                          "Time is money",
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      )),
+                ),
+              ),
+            );
+          }),
+    );
   }
 }
